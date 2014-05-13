15e4eb5335d1c36931aad5df6b64bbcea8ec4584
Konrad Zemek <konrad.zemek@gmail.com>
2014-05-14 01:08:40 +0200

    VFS-556 Add a token_logic module.

---
+++ src/logic/token_logic.erl
@@ -0,0 +1,32 @@
+%% ===================================================================
+%% @author Konrad Zemek
+%% @copyright (C): 2014 ACK CYFRONET AGH
+%% This software is released under the MIT license
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc The module implementing the business logic for tokens created by users.
+%% This module serves as a buffer between the database and the REST API.
+%% @end
+%% ===================================================================
+-module(token_logic).
+-author("Konrad Zemek").
+
+
+%% Atoms representing types of valid tokens.
+-type token_type() :: group_invite_token | space_create_token |
+    space_invite_token | accounts_merge_token | space_support_token.
+
+
+%% API
+-export([is_valid/2]).
+
+
+%% is_valid/2
+%% ====================================================================
+%% @doc Checks if a given token is a valid token of a given type.
+%% ====================================================================
+-spec is_valid(Token :: binary(), TokenType :: token_type()) -> boolean().
+%% ====================================================================
+is_valid(Token, TokenType) ->
+    true.
