5f3882aaadf276c972afe61f20757c79b7f0d09b
Rafał Słota <roxeon@gmail.com>
2014-07-28 21:59:19 +0200

    fix client verify endpoint

--- src/rest_modules/auth_rest_module.erl
+++ src/rest_modules/auth_rest_module.erl
@@ -36,7 +36,7 @@ routes() ->
         {<<"/openid/client/access_code">>, M, S#rstate{resource = acscode, methods = [get]}},
         {<<"/openid/client/tokens">>, M, S#rstate{resource = ctokens, methods = [post, get], noauth = [post]}},
         {<<"/openid/client/tokens/:accessId">>, M, S#rstate{resource = ctoken, methods = [delete]}},
-        {<<"/">>, M, S#rstate{resource = verify, methods = [post]}},
+        {<<"/openid/client/verify">>, M, S#rstate{resource = verify, methods = [post]}},
         {<<"/openid/provider/tokens">>, M, S#rstate{resource = ptokens, methods = [post]}}
     ].
 
