fe4dcbe4ddc0ca914bb22110830fc150b3e370c7
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-08-25 10:21:19 +0200

    VFS-797 Change paths in ct tests.

--- test_distributed/test_utils.hrl
+++ test_distributed/test_utils.hrl
@@ -15,27 +15,42 @@
 -ifndef(TEST_UTILS_HRL).
 -define(TEST_UTILS_HRL, 1).
 
--define(GR_DEPS,[sasl,lager,ssl,erlydtl,ranch,cowlib,cowboy,gproc,xmerl,ibrowse]).
+-define(GR_DEPS, [sasl, lager, ssl, erlydtl, ranch, cowlib, cowboy, gproc, xmerl, ibrowse]).
 
--define(CREATE_DUMMY_AUTH, file:make_dir("gui_static"),file:write_file("gui_static/auth.config",<<"[].">>)).
+-define(MAKE_DIR(Root, Dir),
+    begin
+        lists:foldl(fun(Leaf, Path) ->
+            NewPath = filename:join(Path, Leaf),
+            file:make_dir(NewPath),
+            NewPath
+        end, Root, filename:split(Dir))
+    end).
+
+-define(CREATE_DUMMY_AUTH,
+    begin
+        ?MAKE_DIR(".", "releases/data/gui_static"),
+        file:write_file("releases/data/gui_static/auth.config", <<"[].">>)
+    end).
 
 -define(PREPARE_CERT_FILES(Config),
     begin
-        GRPCADir = filename:join(?config(priv_dir,Config), "releases/data/grpca"),
-        CACertsDir = filename:join(?config(priv_dir,Config), "releases/data/cacerts"),
+        PrivDir = ?config(priv_dir, Config),
+        ?MAKE_DIR(PrivDir, "releases/data"),
+        GRPCADir = filename:join(PrivDir, "releases/data/grpca"),
+        CACertsDir = filename:join(PrivDir, "releases/data/cacerts"),
         [{project_root, ProjectRoot}] = ets:lookup(suite_state, project_root),
-        os:cmd("cp -r "++filename:join(ProjectRoot,"releases/data/grpca")++" "++GRPCADir),
-        os:cmd("cp -r "++filename:join(ProjectRoot,"releases/data/cacerts")++" "++CACertsDir),
-        {CACertsDir,GRPCADir}
+        os:cmd("cp -r " ++ filename:join(ProjectRoot, "grpca") ++ " " ++ GRPCADir),
+        os:cmd("cp -r " ++ filename:join(ProjectRoot, "cacerts") ++ " " ++ CACertsDir),
+        {CACertsDir, GRPCADir}
     end).
 
--define(cert_paths(CACertsDir,GRPCADir),
-    {ca_cert_file,filename:join(CACertsDir,"ca.crt")},
-    {cert_file,filename:join(CACertsDir,"server.crt")},
-    {key_file,filename:join(CACertsDir,"server.key")},
+-define(cert_paths(CACertsDir, GRPCADir),
+    {ca_cert_file, filename:join(CACertsDir, "ca.crt")},
+    {cert_file, filename:join(CACertsDir, "server.crt")},
+    {key_file, filename:join(CACertsDir, "server.key")},
     {grpca_dir, GRPCADir},
-    {rest_cert_file, filename:join(GRPCADir,"rest.pem")},
-    {rest_key_file, filename:join(GRPCADir,"rest_key.pem")}).
+    {rest_cert_file, filename:join(GRPCADir, "rest.pem")},
+    {rest_key_file, filename:join(GRPCADir, "rest_key.pem")}).
 
 
 -endif.
