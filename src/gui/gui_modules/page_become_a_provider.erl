3d489e4f9e4b6905129626c316d452d990d63707
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-09-24 14:05:07 +0200

    VFS-855, estetic changes

--- src/gui/gui_modules/page_become_a_provider.erl
+++ src/gui/gui_modules/page_become_a_provider.erl
@@ -42,7 +42,7 @@ body() ->
             #h3{style = <<"padding: 10px;">>, body = <<"Become a provider">>},
             #p{style = <<"padding: 10px; font-size: 18px; width: 45%; line-height: 22.15px;">>,
                 body = <<"Onedata <strong>provider</strong> can be anyone who decides to share storage space by installing ",
-                "onedata software stack. Afterwards, he can support storage spaces for onedata users.">>,
+                "onedata software stack. Afterwards, he can support storage spaces for onedata users.">>},
 
             #link{class = <<"btn btn-primary">>, url = <<?DOWNLOAD_LINK>>,
                 style = <<"margin: 0 10px 20px; width: 300px;">>,
