258c24f70d851db86ebc013b1d897a0bf9b4fe66
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-10-01 16:53:08 +0200

    VFS-870, change more icons

--- src/gui/gui_modules/page_manage_account.erl
+++ src/gui/gui_modules/page_manage_account.erl
@@ -85,7 +85,7 @@ user_name_section(User) ->
         #span{style = <<"font-size: 18px;">>, id = <<"displayed_name">>, body = Name},
         #link{id = <<"change_name_button">>, class = <<"glyph-link">>, style = <<"margin-left: 10px;">>,
             postback = {action, show_name_edition, [true]}, body =
-            #span{class = <<"fui-new">>, style = <<"font-size: 16px;">>}},
+            #span{class = <<"icomoon-pencil2">>, style = <<"font-size: 16px;">>}},
         #textbox{id = <<"new_name_textbox">>, class = <<"flat">>, body = <<"">>, style = <<"display: none;">>,
             placeholder = <<"New name">>, value = Name},
         #link{id = <<"new_name_submit">>, class = <<"glyph-link">>, style = <<"display: none; margin-left: 10px;">>,
