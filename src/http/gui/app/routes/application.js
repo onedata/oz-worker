28931d8c67527bb62c576f634e8bd4c31a729437
Jakub Liput <jakub.liput@gmail.com>
2016-02-18 16:44:11 +0100

    VFS-1636: Added OP adapter; using stubbed OP authentication

--- src/http/gui/app/routes/application.js
+++ src/http/gui/app/routes/application.js
@@ -5,11 +5,20 @@ let ApplicationRoute = Ember.Route.extend(ApplicationRouteMixin);
 
 export default ApplicationRoute.extend({
   activate() {
+    // examples on registering session events handlers
     this.get('session').on('authenticationSucceeded', () => {
       console.debug('authentication succeeded!');
     });
     this.get('session').on('invalidationSucceeded', () => {
       console.debug('session has been invalidated!');
     });
-  }
+  },
+
+  // TODO: invoked only when application is reloaded!
+  initSession: function() {
+    this.get('session')
+      .authenticate('authenticator:basic').catch((reason) => {
+        console.warn(`you shall NOT pass, because: ${reason}`);
+      });
+  }.on('init')
 });
