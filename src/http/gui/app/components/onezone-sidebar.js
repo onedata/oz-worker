47425ac8c191afd1fbba93ba6d0b5f86753d9908
Jakub Liput <jakub.liput@gmail.com>
2016-03-01 17:11:51 +0100

    VFS-1672: Sticky tooltip for sidebar

--- src/http/gui/app/components/onezone-sidebar.js
+++ src/http/gui/app/components/onezone-sidebar.js
@@ -8,8 +8,19 @@ export default Ember.Component.extend({
   /** AuthAccounts list should be injected */
   authAccounts: null,
 
-  onChange: function() {
-    let a = this.get('authAccounts');
-    console.log(a);
-  }.observes('authAccounts')
+  // didInsertElement() {
+  //   let box = $('.accordion-container');
+  //   let boxElement = box[0];
+  //
+  //   box.mousewheel(function(e) {
+  //     let delta = e.deltaY;
+  //     console.log(box.css('top'));
+  //     let scrollBegin = (parseInt(box.css('top')) === 80);
+  //     let scrollEnd = (parseInt(box.css('top') === (box.height()+80)));
+  //     let isScrollingUp = (delta > 0);
+  //     if (!(scrollBegin && isScrollingUp) && !(scrollEnd && !isScrollingUp)) {
+  //       box.css('top', `${parseInt(box.css('top'))+delta}px`);
+  //     }
+  //   });
+  // }
 });
--- src/http/gui/app/components/spaces-accordion-item.js
+++ src/http/gui/app/components/spaces-accordion-item.js
@@ -8,5 +8,22 @@ export default Ember.Component.extend({
 
   providers: function() {
     return this.get('space').get('providers');
-  }.property('space')
+  }.property('space'),
+
+  cachedSupportToken: null,
+
+  // TODO: maybe this code should be moved somewhere else... (make more global)
+  didInsertElement() {
+    let floater = $('.floater');
+
+    let changePos = function() {
+      let offset = floater.parent().offset();
+      let left = `${parseInt(offset.left) + floater.parent().width()}px`;
+      let top = offset.top;
+      floater.css({left: left, top: top});
+    };
+
+    floater.parent().on('mouseover', changePos);
+    $('.accordion-container').on('scroll', changePos);
+  }
 });
