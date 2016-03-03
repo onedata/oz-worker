7ae747cc511cdb4296d4cc88792a70499faed716
Michal Zmuda <zmu.michal@gmail.com>
2016-03-03 15:28:51 +0100

    VFS-1629 sending info needed by op

--- src/subscriptions/translator.erl
+++ src/subscriptions/translator.erl
@@ -12,37 +12,34 @@
 -author("Michal Zmuda").
 
 -include("datastore/oz_datastore_models_def.hrl").
+-include_lib("ctool/include/logging.hrl").
 
 -export([get_msg/3]).
 
 
 get_msg(Seq, Doc, space) ->
     #document{value = Value, key = ID} = Doc,
-    #space{name = Name, groups = Groups, users = Users} = Value,
-    [{seq, Seq}, {space, [
+    #space{name = Name} = Value,
+    [{seq, Seq}, revs_prop(Doc), {id, ID}, {space, [
         {id, ID},
-        {name, Name},
-        {groups, Groups},
-        {users, Users}
+        {name, Name}
     ]}];
 get_msg(Seq, Doc, user_group) ->
     #document{value = Value, key = ID} = Doc,
-    #user_group{users = Users, name = Name, spaces = Spaces} = Value,
-    [{seq, Seq}, {group, [
-        {id, ID},
-        {name, Name},
-        {spaces, Spaces},
-        {users, Users}
+    #user_group{name = Name} = Value,
+    [{seq, Seq}, revs_prop(Doc), {id, ID}, {group, [
+        {name, Name}
     ]}];
 get_msg(Seq, Doc, onedata_user) ->
     #document{value = Value, key = ID} = Doc,
     #onedata_user{name = Name, spaces = Spaces, groups = Groups} = Value,
-    [{seq, Seq}, {user, [
-        {id, ID},
+    [{seq, Seq}, revs_prop(Doc), {id, ID}, {user, [
         {name, Name},
-        {spaces, Spaces},
-        {groups, Groups}
+        {space_ids, Spaces},
+        {group_ids, Groups}
     ]}];
-
 get_msg(_Seq, _Doc, _Type) ->
     [].
+
+revs_prop(Doc) ->
+    {revs, element(2, Doc#document.rev)}.
