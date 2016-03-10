977dd17d196e2694f1fd7921c08337f605fae7c7
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-03-10 16:41:20 +0100

    VFS-1638 add a debug log

--- test_distributed/connection_test_SUITE.erl
+++ test_distributed/connection_test_SUITE.erl
@@ -29,7 +29,7 @@
 all() -> ?ALL([rest_api_connection_test, datastore_connection_test]).
 
 rest_api_connection_test(Config) ->
-    ct:print("CO DO... ~p", [Config]),
+    ct:print("Config... ~p", [Config]),
     [Node1, Node2] = ?config(oz_worker_nodes, Config),
     {ok, RestPort} = rpc:call(Node1, application, get_env, [?APP_Name, rest_port]),
     URL1 = str_utils:format("https://~s:~B/provider/test/check_my_ip", [utils:get_host(Node1), RestPort]),
@@ -48,6 +48,7 @@ datastore_connection_test(Config) ->
 
 init_per_suite(Config) ->
     NewConfig = ?TEST_INIT(Config, ?TEST_FILE(Config, "env_desc.json")),
+    ct:print("NewConfig ~p", [NewConfig]),
     NewConfig.
 
 init_per_testcase(rest_api_connection_test, Config) ->
