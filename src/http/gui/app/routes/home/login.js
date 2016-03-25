badca7cec5b1bdbf1ef4c60667d08ebd3077449c
Jakub Liput <jakub.liput@gmail.com>
2016-03-25 21:38:47 +0100

    VFS-1768: Zone name fetch from server

--- src/http/gui/app/routes/home/login.js
+++ src/http/gui/app/routes/home/login.js
@@ -8,12 +8,29 @@ export default LoginRoute.extend({
   session: Ember.inject.service('session'),
   onezoneServer: Ember.inject.service('onezoneServer'),
   name: 'login',
-  zoneName: null,
 
   beforeModel() {
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('onezone');
     }
+  },
+
+  model() {
+    return new Ember.RSVP.Promise((resolve/*, reject*/) => {
+      this.get('onezoneServer').getZoneName().then(
+        (zoneName) => {
+          resolve({
+            zoneName: zoneName
+          });
+        },
+        () => {
+          console.error('Failed to get zone name');
+          resolve({
+            zoneName: null,
+          });
+        }
+      );
+    });
   }
 
   //actions: {
