f0d54ece9faebc669feb8eebbe90e9f503706e5e
Jakub Liput <jakub.liput@gmail.com>
2016-04-12 15:03:57 +0200

    VFS-1768: Fixed onezone modals

--- src/http/gui/app/components/onezone-modal-container.js
+++ src/http/gui/app/components/onezone-modal-container.js
@@ -11,26 +11,34 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
 
+  classNames: ['onezone-modal-container'],
+
   /** Should be injected */
   providers: null,
+
+  // isAnyModal: function() {
+  //   return this.get('modalFirstLogin') ||
+  //     this.get('modalNoneProviders') || this.get('modalGetSupport');
+  // }.property('modalFirstLogin', 'modalNoneProviders', 'modalGetSupport'),
 
   isFirstLogin: function() {
     let sessionDetails = this.get('session').get('sessionDetails');
     return sessionDetails && sessionDetails.firstLogin;
   }.property('session.sessionDetails'),
 
-  /** If true, show "none providers" modal */
-  modalNoneProviders: function() {
-    return !this.get('isFirstLogin') &&
-      (this.get('providers').filterBy('isWorking', true).length === 0);
-  }.property('providers.@each.isWorking', 'isFirstLogin'),
-
   modalFirstLogin: function() {
     return this.get('isFirstLogin');
   }.property('isFirstLogin'),
 
   modalGetSupport: function() {
-    return !this.get('isFirstLogin') &&
-      (this.get('providers').get('length') === 0);
-  }.property('providers', 'isFirstLogin')
+    return !this.get('isFirstLogin') && (!this.get('providers') ||
+      (this.get('providers').get('length') === 0));
+  }.property('providers', 'providers.length', 'isFirstLogin'),
+
+  /** If true, show "none providers" modal */
+  modalNoneProviders: function() {
+    return !this.get('modalGetSupport') && (!this.get('providers') ||
+      (this.get('providers').filterBy('isWorking', true).length === 0));
+  }.property('providers.@each.isWorking', 'modalGetSupport'),
+
 });
