47362f847630d80ba453a7a5e462a5c6f5d88681
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-04-20 15:13:28 +0200

    Squashed 'bamboos/' changes from 1650ebd..639071d
    639071d VFS-1825 some code and comments polishing
    b67b27f VFS-1825 merge with develop
    9962ad8 VFS-1825 move common gui logic to one file
    db25dbc Merge pull request #114 in VFS/bamboos from bugfix/VFS-1949-fix-containers-hostnames-in-env to develop
    c010260 VFS-1825 fix broken livereload
    6e6f674 VFS-1949 Replace '_' with '-' in containers hostnames.
    c51543f VFS-1825 modify user persistence level, remove unused print in make.py
    166067c Merge pull request #110 in VFS/bamboos from feature/VFS-1751-create-amazon-iam-api-mock to develop
    1e57ada Merge branch 'develop' into feature/VFS-1751-create-amazon-iam-api-mock
    b98b765 Merge pull request #109 in VFS/bamboos from feature/VFS-1749-create-docker-up-script-for-python to develop
    3d071d9 VFS-1751 Amazon IAM API mock
    dcf3864 Merge branch 'develop' into feature/VFS-1749-create-docker-up-script-for-python
    ee7785e VFS-1749 Python LUMA start script
    c1308b1 Merge pull request #111 in VFS/bamboos from feature/VFS-1743-acceptance-tests-for-many-providers to develop
    0cd46b8 Merge pull request #113 in VFS/bamboos from feature/VFS-1760-no-separate-docker-up-develop to develop
    608a9c2 Merge remote-tracking branch 'origin/develop' into feature/VFS-1743-acceptance-tests-for-many-providers
    a50b96d Merge branch 'develop' of ssh://git.plgrid.pl:7999/vfs/bamboos into hotfix/VFS-1743-acceptance-tests-for-many-providers
    2f4c953 Merge remote-tracking branch 'origin/develop' into hotfix/VFS-1743-acceptance-tests-for-many-providers
    9cce7f7 Merge remote-tracking branch 'origin/develop' into hotfix/VFS-1743-acceptance-tests-for-many-providers
    67a95d8 VFS-1743 - fix mounting storage on host in wrong directory
    c96e192 VFS-1743 - fix wrong key for oz_domain name
    
    git-subtree-dir: bamboos
    git-subtree-split: 639071dfd54c60eeb46b7e455ba3b3da2b4c1213

--- docker/environment/provider_worker.py
+++ docker/environment/provider_worker.py
@@ -8,14 +8,17 @@ Brings up a set of oneprovider worker nodes. They can create separate clusters.
 import os
 import subprocess
 import sys
-from . import common, docker, worker, gui_livereload
+from . import common, docker, worker, gui
 
 DOCKER_BINDIR_PATH = '/root/build'
 
 
-def up(image, bindir, dns_server, uid, config_path, logdir=None, storages_dockers=None):
+def up(image, bindir, dns_server, uid, config_path, logdir=None,
+       storages_dockers=None, luma_config=None):
     return worker.up(image, bindir, dns_server, uid, config_path,
-                     ProviderWorkerConfigurator(), logdir, storages_dockers)
+                     ProviderWorkerConfigurator(), logdir,
+                     storages_dockers=storages_dockers,
+                     luma_config=luma_config)
 
 
 class ProviderWorkerConfigurator:
@@ -24,92 +27,59 @@ class ProviderWorkerConfigurator:
         if 'oz_domain' in sys_config:
             oz_hostname = worker.cluster_domain(sys_config['oz_domain'], uid)
             sys_config['oz_domain'] = oz_hostname
-        # If livereload bases on gui output dir mount, change the location
-        # from where static files are served to that dir.
-        if 'gui_livereload' in cfg:
-            if cfg['gui_livereload'] in ['mount_output', 'mount_output_poll']:
-                sys_config['gui_static_files_root'] = {
-                    'string': '/root/gui_static'}
         return cfg
 
     def pre_start_commands(self, domain):
         return 'escript bamboos/gen_dev/gen_dev.escript /tmp/gen_dev_args.json'
 
-        # Called BEFORE the instance (cluster of workers) is started
+    # Called BEFORE the instance (cluster of workers) is started
     def pre_configure_instance(self, instance, uid, config):
         this_config = config[self.domains_attribute()][instance]
-        print('GUI for: {0}'.format(common.format_hostname(instance, uid)))
-        # Prepare static dockers with GUI (if required) - they are reused by
-        # whole cluster (instance)
+        if 'gui_override' in this_config and isinstance(
+                this_config['gui_override'], dict):
+            # Preconfigure GUI override
+            gui_config = this_config['gui_override']
+            hostname = common.format_hostname(instance, uid)
+            gui.override_gui(gui_config, instance, hostname)
+
+    # Called AFTER the instance (cluster of workers) has been started
+    def post_configure_instance(self, bindir, instance, config, container_ids,
+                                output, storages_dockers=None,
+                                luma_config=None):
+        this_config = config[self.domains_attribute()][instance]
+        # Check if gui livereload is enabled in env and turn it on
         if 'gui_override' in this_config and isinstance(
                 this_config['gui_override'], dict):
             gui_config = this_config['gui_override']
-            mount_path = gui_config['mount_path']
-            print('    static root: {0}'.format(mount_path))
-            if 'host' in gui_config['mount_from']:
-                host_volume_path = gui_config['mount_from']['host']
-                print('    from host:   {0}'.format(host_volume_path))
-            elif 'docker' in gui_config['mount_from']:
-                static_docker_image = gui_config['mount_from']['docker']
-                # Create volume name from docker image name and instance name
-                volume_name = self.gui_files_volume_name(
-                    static_docker_image, instance)
-                # Create the volume from given image
-                docker.create_volume(
-                    path=mount_path,
-                    name=volume_name,
-                    image=static_docker_image,
-                    command='/bin/true')
-                print('    from docker: {0}'.format(static_docker_image))
             livereload_flag = gui_config['livereload']
-            print('    livereload:  {0}'.format(livereload_flag))
-        else:
-            print('    config not found, skipping')
-
-    # Called AFTER the instance (cluster of workers) has been started
-    def post_configure_instance(self, bindir, instance, config,
-                                   container_ids, output, storages_dockers=None):
-        this_config = config[self.domains_attribute()][instance]
-        # Check if gui_livereload is enabled in env and turn it on
-        if 'gui_override' in config and isinstance(config['gui_override'],
-                                                   dict):
-            gui_config = this_config['gui_override']
-            livereload_flag = gui_config['livereload']
-            livereload_dir = gui_config['mount_path']
             if livereload_flag:
                 for container_id in container_ids:
-                    gui_livereload.run(container_id, livereload_dir)
+                    livereload_dir = gui_config['mount_path']
+                    gui.run_livereload(container_id, livereload_dir)
         if 'os_config' in this_config:
             os_config = this_config['os_config']
             create_storages(config['os_configs'][os_config]['storages'],
                             output[self.nodes_list_attribute()],
-                            this_config[self.app_name()], bindir, storages_dockers)
+                            this_config[self.app_name()], bindir,
+                            storages_dockers)
 
     def extra_volumes(self, config, bindir, instance):
         if 'os_config' in config and config['os_config']['storages']:
             if isinstance(config['os_config']['storages'][0], basestring):
                 posix_storages = config['os_config']['storages']
             else:
-                posix_storages = [s['name'] for s in config['os_config']['storages']
+                posix_storages = [s['name'] for s in
+                                  config['os_config']['storages']
                                   if s['type'] == 'posix']
         else:
             posix_storages = []
 
         extra_volumes = [common.volume_for_storage(s) for s in posix_storages]
-        # Check if gui mount is enabled in env and add required volumes
-        if 'gui_override' in config and isinstance(config['gui_override'], dict):
+        # Check if gui override is enabled in env and add required volumes
+        if 'gui_override' in config and isinstance(config['gui_override'],
+                                                   dict):
             gui_config = config['gui_override']
-            if 'host' in gui_config['mount_from']:
-                # Mount a path on host to static root dir on OZ docker
-                mount_path = gui_config['mount_path']
-                host_volume_path = gui_config['mount_from']['host']
-                extra_volumes.append((host_volume_path, mount_path, 'ro'))
-            elif 'docker' in gui_config['mount_from']:
-                static_docker_image = gui_config['mount_from']['docker']
-                # Create volume name from docker image name
-                volume_name = self.gui_files_volume_name(
-                    static_docker_image, instance)
-                extra_volumes.append({'volumes_from': volume_name})
+            extra_volumes.extend(gui.extra_volumes(gui_config, instance))
         return extra_volumes
 
     def app_name(self):
@@ -123,12 +93,6 @@ class ProviderWorkerConfigurator:
 
     def nodes_list_attribute(self):
         return "op_worker_nodes"
-
-    # Create volume name from docker image name and instance name
-    def gui_files_volume_name(self, image_name, instance_name):
-        volume_name = image_name.split('/')[-1].replace(
-            ':', '_').replace('-', '_')
-        return '{0}_{1}'.format(instance_name, volume_name)
 
 
 def create_storages(storages, op_nodes, op_config, bindir, storages_dockers):
@@ -147,8 +111,9 @@ def create_storages(storages, op_nodes, op_config, bindir, storages_dockers):
     container = first_node.split("@")[1]
     worker_name = container.split(".")[0]
     cookie = op_config[worker_name]['vm.args']['setcookie']
-    script_paths = dict(map(lambda (k, v): (k, os.path.join(DOCKER_BINDIR_PATH, v)),
-                              script_names.iteritems()))
+    script_paths = dict(
+        map(lambda (k, v): (k, os.path.join(DOCKER_BINDIR_PATH, v)),
+            script_names.iteritems()))
     for storage in storages:
         if isinstance(storage, basestring):
             storage = {'type': 'posix', 'name': storage}
@@ -163,19 +128,23 @@ def create_storages(storages, op_nodes, op_config, bindir, storages_dockers):
             pool = storage['pool'].split(':')[0]
             command = ['escript', script_paths['ceph'], cookie,
                        first_node, storage['name'], "ceph",
-                       config['host_name'], pool, config['username'], config['key']]
+                       config['host_name'], pool, config['username'],
+                       config['key']]
             assert 0 is docker.exec_(container, command, tty=True,
                                      stdout=sys.stdout, stderr=sys.stderr)
         elif storage['type'] == 's3':
             config = storages_dockers['s3'][storage['name']]
             command = ['escript', script_paths['s3'], cookie,
                        first_node, storage['name'], config['host_name'],
-                       storage['bucket'], config['access_key'], config['secret_key'],
-                       "iam.amazonaws.com"]
+                       storage['bucket'], config['access_key'],
+                       config['secret_key'],
+                       config.get('iam_request_scheme', 'https'),
+                       config.get('iam_host', 'iam.amazonaws.com')]
             assert 0 is docker.exec_(container, command, tty=True,
                                      stdout=sys.stdout, stderr=sys.stderr)
         else:
-            raise RuntimeError('Unknown storage type: {}'.format(storage['type']))
+            raise RuntimeError(
+                'Unknown storage type: {}'.format(storage['type']))
     # clean-up
     for _, script_name in script_names.iteritems():
         command = ['rm', os.path.join(bindir, script_name)]
