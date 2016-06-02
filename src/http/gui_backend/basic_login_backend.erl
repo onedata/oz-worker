7f4eb1babb761999af1caf474ff078585be4f285
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-06-02 11:13:36 +0200

    VFS-2111 Implement basic login backend

--- src/http/gui_backend/basic_login_backend.erl
+++ src/http/gui_backend/basic_login_backend.erl
@@ -32,4 +32,12 @@
 %%--------------------------------------------------------------------
 -spec page_init() -> gui_html_handler:page_init_result().
 page_init() ->
-    ok.
+    Req = g_ctx:get_cowboy_req(),
+    {Header, _} = cowboy_req:header(<<"authorization">>, Req),
+    case user_logic:authenticate_by_basic_credentials(Header) of
+        {ok, UserId} ->
+            g_session:log_in(UserId),
+            {reply, 200};
+        _ ->
+            {reply, 401}
+    end.
