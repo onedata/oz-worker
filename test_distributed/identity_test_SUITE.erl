5a7b3b87ee5bfdf49e01c0d42d1d94e8486c1be5
Michal Zmuda <zmu.michal@gmail.com>
2016-08-23 10:49:49 +0200

    VFS-2309 no more bootstrap on testmaster

--- test_distributed/identity_test_SUITE.erl
+++ test_distributed/identity_test_SUITE.erl
@@ -97,14 +97,7 @@ certs_are_publishable_via_rest(Config) ->
 %%%===================================================================
 
 init_per_suite(Config) ->
-    EnvDescFile = ?TEST_FILE(Config, "env_desc.json"),
-    Temp = utils:mkdtemp(),
-    Adjusted = location_service_test_utils:adjust_env_desc(Temp, EnvDescFile),
-    location_service_test_utils:start(),
-
-    RunningConfig = ?TEST_INIT(Config, Adjusted),
-    utils:rmtempdir(Temp),
-    RunningConfig.
+    ?TEST_INIT(Config, ?TEST_FILE(Config, "env_desc.json")).
 
 init_per_testcase(_, _Config) ->
     application:start(etls),
@@ -117,7 +110,6 @@ end_per_testcase(_, _Config) ->
     ok.
 
 end_per_suite(Config) ->
-    location_service_test_utils:stop(),
     test_node_starter:clean_environment(Config).
 
 
