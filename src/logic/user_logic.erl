dc624d920ce74cc1e438c5169df4f5ddf169c21e
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2016-08-24 17:51:35 +0200

    VFS-2156 Change onepanel config env variables types

--- src/logic/user_logic.erl
+++ src/logic/user_logic.erl
@@ -537,11 +537,7 @@ clean_space_name_mapping(UserId, SpaceId) ->
     Password :: binary()) -> {ok, UserDoc :: #document{}} | {error, term()}.
 authenticate_by_basic_credentials(Login, Password) ->
     Headers = [basic_auth_header(Login, Password)],
-    {ok, OnepanelRESTURL} =
-        application:get_env(?APP_Name, onepanel_rest_url),
-    {ok, OnepanelGetUsersEndpoint} =
-        application:get_env(?APP_Name, onepanel_users_endpoint),
-    URL = <<OnepanelRESTURL/binary, OnepanelGetUsersEndpoint/binary, Login/binary>>,
+    URL = get_onepanel_rest_user_url(Login),
     RestCallResult = case http_client:get(URL, Headers, <<"">>, [insecure]) of
         {ok, 200, _, JSON} ->
             json_utils:decode(JSON);
@@ -628,11 +624,7 @@ change_user_password(Login, OldPassword, NewPassword) ->
         {<<"content-type">>, <<"application/json">>},
         basic_auth_header(Login, OldPassword)
     ],
-    {ok, OnepanelRESTURL} =
-        application:get_env(?APP_Name, onepanel_rest_url),
-    {ok, OnepanelGetUsersEndpoint} =
-        application:get_env(?APP_Name, onepanel_users_endpoint),
-    URL = <<OnepanelRESTURL/binary, OnepanelGetUsersEndpoint/binary, Login/binary>>,
+    URL = get_onepanel_rest_user_url(Login),
     Body = json_utils:encode([{<<"password">>, NewPassword}]),
     case http_client:put(URL, Headers, Body, [insecure]) of
         {ok, 204, _, _} ->
@@ -670,6 +662,20 @@ basic_auth_header(Login, Password) ->
     UserAndPassword = base64:encode(<<Login/binary, ":", Password/binary>>),
     {<<"Authorization">>, <<"Basic ", UserAndPassword/binary>>}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Returns onepanel REST endpoint for user management.
+%% @end
+%%--------------------------------------------------------------------
+-spec get_onepanel_rest_user_url(Login :: binary()) -> URL :: binary().
+get_onepanel_rest_user_url(Login) ->
+    {ok, OnepanelRESTURL} =
+        application:get_env(?APP_Name, onepanel_rest_url),
+    {ok, OnepanelGetUsersEndpoint} =
+        application:get_env(?APP_Name, onepanel_users_endpoint),
+    <<(str_utils:to_binary(OnepanelRESTURL))/binary,
+        (str_utils:to_binary(OnepanelGetUsersEndpoint))/binary, Login/binary>>.
 
 %%--------------------------------------------------------------------
 %% @private
