0ea69a47d46fb3c229bdcfb30696fceb30125ab7
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-09-19 14:32:17 +0200

    VFS-2397 - oai_verb_behaviour and oai_errors docs and specs

--- src/http/oai_pmh/oai_errors.erl
+++ src/http/oai_pmh/oai_errors.erl
@@ -5,6 +5,7 @@
 %%% cited in 'LICENSE.txt'.
 %%% @doc
 %%% This module is responsible for handling OAI-PMH errors and exceptions.
+%%% http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#ErrorConditions
 %%% @end
 %%%-------------------------------------------------------------------
 -module(oai_errors).
@@ -15,14 +16,19 @@
 %% API
 -export([handle/1]).
 
-%%TODO write spec
 
-handle({ErrorCode, Description}) ->
-    ?OAI_ERROR(ErrorCode, Description);
+%%--------------------------------------------------------------------
+%% @doc
+%% Function responsible for handling OAI-PMH errors.
+%% @end
+%%--------------------------------------------------------------------
+-spec handle({oai_error_code(), list()} | oai_error_code()) -> oai_error().
+handle({ErrorCode, Description}) -> ?OAI_ERROR(ErrorCode, Description);
 handle(badVerb) -> ?BAD_VERB;
 handle(badArgument) -> ?BAD_ARGUMENT;
 handle(badResumptionToken) -> ?BAD_RESUMPTION_TOKEN;
 handle(cannotDisseminateFormat) -> ?CANNOT_DISSEMINATE_FORMAT;
 handle(idDoesNotExist) -> ?ID_DOES_NOT_EXIST;
 handle(noRecordsMatch) -> ?NO_RECORDS_MATCH;
-handle(noMetadataFormats) -> ?NO_METADATA_FORMATS.
+handle(noMetadataFormats) -> ?NO_METADATA_FORMATS;
+handle(noSetHierarchy) -> ?NO_SET_HIERARCHY.
--- src/http/oai_pmh/oai_verb_behaviour.erl
+++ src/http/oai_pmh/oai_verb_behaviour.erl
@@ -4,7 +4,8 @@
 %%% This software is released under the MIT license
 %%% cited in 'LICENSE.txt'.
 %%% @doc
-%%% WRITEME
+%%% The behaviour that must be implemented by each module responsible
+%%% for handling OAI-PMH requests for given OAI-PMH verb
 %%% @end
 %%%-------------------------------------------------------------------
 -module(oai_verb_behaviour).
@@ -12,15 +13,60 @@
 
 -include("http/handlers/oai.hrl").
 
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Returns list of arguments that are required for given request.
+%%& If such argument is not passed, request will be responded with
+%%% badArgument error code.
+%%% @end
+%%%-------------------------------------------------------------------
 -callback required_arguments() -> [binary()].
 
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Returns list of arguments that are optional for given request.
+%%% @end
+%%%-------------------------------------------------------------------
 -callback optional_arguments() -> [binary()].
 
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Returns list of exclusive arguments for given request.
+%%% Exclusive argument must be the only argument (except verb argument).
+%%% @end
+%%%-------------------------------------------------------------------
 -callback exclusive_arguments() -> [binary()].
 
--callback required_response_elements() -> [atom()].
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Returns list of XML elements that are required in OIA-PMH response.
+%%% @end
+%%%-------------------------------------------------------------------
+-callback required_response_elements() -> [binary()].
 
--callback optional_response_elements() -> [atom()].
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Returns list of XML elements that are optional in OIA-PMH response.
+%%% @end
+%%%-------------------------------------------------------------------
+-callback optional_response_elements() -> [binary()].
 
--callback get_element(Element :: atom(), Args :: [{Key :: binary(), Value :: binary()}])
-        -> binary() | [binary()] | #xmlElement{} | [#xmlElement{}].
+
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Function responsible for generating response to given request.
+%%% If generating response fails, function must throw exception in ont
+%%% of the following ways:
+%%%     throw:ErrorCode
+%%%     or
+%%%     throw:{ErrorCode, Description}
+%%% where ErrorCode is an atom defining error code as defined in OAI-PMH
+%%% documentation available on:
+%%% http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#ErrorConditions
+%%% Error will be handled by oai_errors module.
+%%% This function must be defined for all elements returned by
+%%% required_response_elements().
+%%% @end
+%%%-------------------------------------------------------------------
+
+-callback get_response(Element :: binary(), Args :: proplist()) -> oai_response().
