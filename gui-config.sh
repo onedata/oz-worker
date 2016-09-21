65f3f7cd0bb9343eec6b186a519b0c75ddccf876
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-09-21 15:08:27 +0200

    VFS-2607 Update gui ref

--- gui-config.sh
+++ gui-config.sh
@@ -21,6 +21,6 @@
 TARGET_DIR='_build/default/lib/gui_static'
 # Image which will be used by default to get the static files. If it cannot
 # be resolved, the script will fall back to secondary.
-PRIMARY_IMAGE='docker.onedata.org/oz-gui-default:VFS-2624'
+PRIMARY_IMAGE='docker.onedata.org/oz-gui-default:VFS-2626'
 # Image which will be used if primary image is not resolved.
-SECONDARY_IMAGE='onedata/oz-gui-default:VFS-2624'
+SECONDARY_IMAGE='onedata/oz-gui-default:VFS-2626'
