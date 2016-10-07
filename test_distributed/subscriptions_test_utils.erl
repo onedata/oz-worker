d40383d68d107f8b0c31dfb267e3ac5fbe94fc18
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-10-07 17:46:04 +0200

    VFS-2659 Update ctool ref

--- test_distributed/subscriptions_test_utils.erl
+++ test_distributed/subscriptions_test_utils.erl
@@ -396,7 +396,7 @@ verify_messages(Context, Retries, Expected, Forbidden) ->
     call_worker(Node, {update_missing_seq, ProviderId, ResumeAt, Missing}),
     All = lists:append(get_messages()),
 
-    ct:print("ALL: ~p~nEXPECTED: ~p~nFORBIdDEN: ~p~n", [All, Expected, Forbidden]),
+%%    ct:print("ALL: ~p~nEXPECTED: ~p~nFORBIdDEN: ~p~n", [All, Expected, Forbidden]),
 
     Seqs = extract_seqs(All),
     NextResumeAt = largest([ResumeAt | Seqs]),
