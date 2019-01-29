22f73ceac059a17d3111e461996bc4a6c1dec266
Michał Stanisz <mch.stanisz@gmail.com>
2019-01-29 12:01:53 +0100

    VFS-5229 Add placeholders to 18.02 for new CT tests implemented in 18.07

---
+++ test_distributed/space_harvesters_api_test_SUITE.erl
@@ -0,0 +1,39 @@
+%%%-------------------------------------------------------------------
+%%% @author Michal Stanisz
+%%% @copyright (C) 2019 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Empty test SUITE to be implemented in version 18.07.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(space_harvesters_api_test_SUITE).
+-author("Michal Stanisz").
+
+-include("registered_names.hrl").
+-include_lib("ctool/include/test/test_utils.hrl").
+-include_lib("ctool/include/logging.hrl").
+-include_lib("ctool/include/test/assertions.hrl").
+-include_lib("ctool/include/test/performance.hrl").
+
+%% API
+-export([all/0]).
+-export([dummy_test/1]).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+all() -> ?ALL([dummy_test]).
+
+dummy_test(_Config) ->
+    ok.
+
+
+
+
+
+
+
