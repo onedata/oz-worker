c18cd1f29c2e303856c417cf0c80a49f983619b6
Jakub Liput <jakub.liput@gmail.com>
2019-05-22 16:32:07 +0200

    VFS-5425 GUI update * VFS-5425 Changed WebSocket authorization to use handshake

--- gui-config.sh
+++ gui-config.sh
@@ -17,6 +17,6 @@
 # Path relative to this script, to which static GUI package will be copied.
 TARGET_PATH='_build/default/lib/gui_static.tar.gz'
 # Image which will be used by default to get the static files.
-PRIMARY_IMAGE='docker.onedata.org/onezone-gui:ID-7ad5d25852'
+PRIMARY_IMAGE='docker.onedata.org/onezone-gui:ID-23d24b4450'
 # Image which will be used if the primary image cannot be resolved.
-SECONDARY_IMAGE='onedata/onezone-gui:ID-7ad5d25852'
+SECONDARY_IMAGE='onedata/onezone-gui:ID-23d24b4450'
