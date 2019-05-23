cf6f283242a44574086b2b14bbdce1c162e03fd5
Jakub Liput <jakub.liput@gmail.com>
2019-05-23 11:33:41 +0200

    VFS-5423 GUI update * VFS-5423 Unfied GUI Service Pack 3

--- gui-config.sh
+++ gui-config.sh
@@ -17,6 +17,6 @@
 # Path relative to this script, to which static GUI package will be copied.
 TARGET_PATH='_build/default/lib/gui_static.tar.gz'
 # Image which will be used by default to get the static files.
-PRIMARY_IMAGE='docker.onedata.org/onezone-gui:ID-23d24b4450'
+PRIMARY_IMAGE='docker.onedata.org/onezone-gui:ID-d3fea02ef7'
 # Image which will be used if the primary image cannot be resolved.
-SECONDARY_IMAGE='onedata/onezone-gui:ID-23d24b4450'
+SECONDARY_IMAGE='onedata/onezone-gui:ID-d3fea02ef7'
