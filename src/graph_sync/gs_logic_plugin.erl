72721d04f175cc573fe2cf04d85b4d994c075816
Łukasz Opioła <lukasz.opiola@gmail.com>
2019-10-18 19:07:25 +0200

    Add oz-worker type support to graph sync

--- src/graph_sync/gs_logic_plugin.erl
+++ src/graph_sync/gs_logic_plugin.erl
@@ -257,4 +257,5 @@ is_type_supported(#gri{type = od_handle}) -> true;
 is_type_supported(#gri{type = od_cluster}) -> true;
 is_type_supported(#gri{type = od_harvester}) -> true;
 is_type_supported(#gri{type = od_token}) -> true;
+is_type_supported(#gri{type = oz_worker}) -> true;
 is_type_supported(#gri{type = _}) -> false.
